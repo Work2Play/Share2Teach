c67cfa7e7ae51df6ad0ce7dd42602c90c68899b2
Iwann10 <iwann@icloud.com>
2024-07-26 17:10:14 +0200

    Starting react app

--- front-end/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-// src/api/axios.js
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'http://localhost:5000/api', // Update this with your .NET API URL
-});
-
-export default instance;
