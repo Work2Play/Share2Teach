7c1bab5f1329a2c248f95414dd4e28ce534ccadd
TrayTrike <traz.mocke@gmail.com>
2024-07-31 14:56:53 +0200

    fixed the compilded with warnings errors for the pages

--- front-end/src/BusinessStudiesPage.js
+++ front-end/src/BusinessStudiesPage.js
@@ -12,14 +12,14 @@ import './BusinessStudiesPage.css';
 const businessIntroduction = "Welcome to our Business Studies Resources page, a treasure trove of educational materials crafted with care by students from the Faculty of Education at North-West University. Our collection is diverse, ranging from detailed explanations on specific topics to comprehensive semester-long planning guides. Designed with a focus on self-directed learning, these resources incorporate cooperative learning and project-based learning strategies to foster a more engaging and effective learning environment. While all materials have undergone peer evaluation to ensure quality, we value your feedback to further enhance their effectiveness. Please take a moment to complete the evaluation form (at the bottom of the page) for any resource you use. Your insights are crucial in identifying areas for improvement and recognizing outstanding contributions. These open educational resources (OER) are freely available, reflecting our commitment to accessible and collaborative education.";
 
 function BusinessStudiesPage() {
-    //the constrant that will store all the values for maths
+    //the constrant that will store all the values
     const [resources, setResources] = useState([])
-
-    //a collection that will store the overall values of Math_Main
-    const resourceCollectionRef = collection(db, "PDFS", "Business_Main", "business")
 
     //making use of use effect sothat it is more dynamic
     useEffect(() => {
+        //a collection that will store the overall values of Business_Main
+        const resourceCollectionRef = collection(db, "PDFS", "Business_Main", "business")
+
         const getResources = async () => {
             //reading the resources
             try {
