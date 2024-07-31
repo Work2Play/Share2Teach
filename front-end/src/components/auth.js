03dc3de9a58b99fa330a48c3657584dd7e9e9800
TrayTrike <traz.mocke@gmail.com>
2024-07-31 11:52:58 +0200

    Added auth login stuff
    added auth stuff, moved firebase.js into a config folder
    the auth stuff allows user to login and logout

---
+++ front-end/src/components/auth.js
@@ -0,0 +1,42 @@
+//all related to login and logout button
+import { useState } from "react";
+
+import { auth, googleProvider } from ".../config/firebase"
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+
+export const Auth = () => {
+    //gets the user that is currently logged in
+    const [logedIn, setLoggedIn] = useState("");
+
+    //creates a popup window for user to sign in with their google account
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider)
+            setLoggedIn(auth.currentUser.email)
+        } catch (err)
+        {
+            console.error(err);
+        }
+    }
+
+    //created a button for user to logout from current account
+    const logout = async () => {
+        try {
+            await signOut(auth)
+            setLoggedIn( )
+        } catch (err)
+        {
+            console.error(err);
+        }
+
+        //creating basic display for the buttons
+        return (
+            <div>
+                <button className="login-button" onclick={signInWithGoogle}>Login</button>
+                <h1 className="userLoged">{logedIn}</h1>
+                <button className="logout-button" onclick={logout}>Logout</button>
+            </div>
+
+
+        )
+}
